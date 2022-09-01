a07c806be51092050d59ead141524a642de1939e
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2022-09-01 16:12:51 +0430

    useing shel component for composition of project

--- src/components/ExpensesItem.js
+++ src/components/ExpensesItem.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import Card from './Card'
 import ExpensesDate from './ExpensesDate'
 import "./ExpensesItem.css"
 function ExpensesItem(props) {
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
         <ExpensesDate date = {props.date} />
         <div className='expense-item__description'>
           <h2>{props.title}</h2>
           <div className='expense-item__price'>${props.amount}</div>
         </div>
-    </div>
+    </Card>
   )
 }
 
