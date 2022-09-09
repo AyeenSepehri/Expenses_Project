a29a426d23b43d97433d94fb11f872dec68fec7b
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2022-09-09 18:27:24 +0430

    define a state and a function to expenses component to manage state of filtering from filter component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,23 @@
+import React , { useState } from 'react'
 import ExpensesItem from './ExpensesItem'
 import "./Expenses.css"
 import Card from '../UI/Card'
+import ExpensesFilter from './ExpensesFilter'
 
 function Expenses(props) {
+
+    //state of drop down filter
+    const [year , setYear] = useState("")
+
+    //function of takeing datas of drop down filter
+    const onYear = (years) => {
+        setYear(years)
+    }
+    console.log(year)
+    
     return (
         <Card className='expenses'>
+            <ExpensesFilter yearValue = {year} yearPicker = {onYear} />
             <ExpensesItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
             <ExpensesItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
             <ExpensesItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />
