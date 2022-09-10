1a92ac938c278efc3f28971cffbb7a3b133785a5
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2022-09-10 12:04:36 +0430

    deleteing test state from component

--- src/components/Expenses/ExpensesItem.js
+++ src/components/Expenses/ExpensesItem.js
@@ -1,22 +1,16 @@
-import React , { useState } from 'react'
+import React from 'react'
 import Card from '../UI/Card'
 import ExpensesDate from './ExpensesDate'
 import "./ExpensesItem.css"
 
 function ExpensesItem(props) {
-  const [title , setTitle] = useState(props.title)
-  const clickHandler = () => {
-    setTitle("state Updated!")
-    console.log("updated!")
-  }
 
   return (
     <Card className='expense-item'>
         <ExpensesDate date = {props.date} />
         <div className='expense-item__description'>
-          <h2>{title}</h2>
+          <h2>{props.title}</h2>
           <div className='expense-item__price'>${props.amount}</div>
-          <button onClick={clickHandler}>clicked!</button>
         </div>
     </Card>
   )
