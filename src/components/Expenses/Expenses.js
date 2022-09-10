ce73019e9aa2641a3929f79211798e9c357a21c4
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2022-09-10 12:08:31 +0430

    modify hard code jsx to a dynamic experition with .map()

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,4 +1,4 @@
-import React , { useState } from 'react'
+import React, { useState } from 'react'
 import ExpensesItem from './ExpensesItem'
 import "./Expenses.css"
 import Card from '../UI/Card'
@@ -7,21 +7,17 @@ import ExpensesFilter from './ExpensesFilter'
 function Expenses(props) {
 
     //state of drop down filter
-    const [year , setYear] = useState("")
+    const [year, setYear] = useState("")
 
     //function of takeing datas of drop down filter
     const onYear = (years) => {
         setYear(years)
     }
-    console.log(year)
-    
+
     return (
         <Card className='expenses'>
-            <ExpensesFilter yearValue = {year} yearPicker = {onYear} />
-            <ExpensesItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
-            <ExpensesItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
-            <ExpensesItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />
-            <ExpensesItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date} />
+            <ExpensesFilter yearValue={year} yearPicker={onYear} />
+            {props.items.map(expenses => <ExpensesItem title={expenses.title} amount={expenses.amount} date={expenses.date} />)}
         </Card>
     )
 }
