6df54d6f542adb7fe9db68bff7540e9228af6769
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2022-09-13 18:13:25 +0430

    cleaning codes and put comment for chartBar component

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import "./chartBar.css"
 
 function ChartBar(props) {
 
+  //define dynamic height for chart bar filles
     let barFillHeight = "0%"
-
     if(props.maxValue > 0){
         barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
     }
@@ -11,7 +12,7 @@ function ChartBar(props) {
   return (
     <div className='chart-bar'>
         <div className='chart-bar__inner'>
-            <div className='chart-bar__fill' style={{height: barFillHeight}}></div>
+            <div className='chart-bar__fill' style={{ height: barFillHeight }}></div>
         </div>
         <div className='chart-bar__label'>{props.label}</div>
     </div>
