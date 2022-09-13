b178c445ad2bba07dee0027528eb043b5e374c44
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2022-09-13 18:15:14 +0430

    set and import ExpensesChart component into Expenses component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import "./Expenses.css"
 import Card from '../UI/Card'
 import ExpensesFilter from './ExpensesFilter'
 import ExpensesList from './ExpensesList'
+import ExpensesChart from './ExpensesChart'
 
 function Expenses(props) {
 
@@ -26,6 +27,7 @@ function Expenses(props) {
     return (
         <Card className='expenses'>
             <ExpensesFilter yearValue={year} yearPicker={onYear} />
+            <ExpensesChart expenses={filteredExpenses}/>
             <ExpensesList items = {filteredExpenses}/>
         </Card>
     )
