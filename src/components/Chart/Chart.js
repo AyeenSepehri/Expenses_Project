8ddd055534cf4629892c873e2ac93a8474705ecc
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2022-09-13 18:11:58 +0430

    takeing maxiumum value of amounts

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,11 +2,18 @@ import React from 'react'
 import ChartBar from './ChartBar'
 import "./Chart.css"
 
-function Chart() {
+function Chart(props) {
+
+  //transform the object of number to an array of number
+  const dataPointsValue = props.dataPoints.map(dataPoint => dataPoint.value)
+
+  //take maximum of amounts and pull them from array with spread operatore
+  const totalMaximum = Math.max(...dataPointsValue)
+  
   return (
     <div className='chart'>
         {props.dataPoints.map((dataPoint) => 
-        <ChartBar value={dataPoint.value} label={dataPoint.label} maxValue={null} key={dataPoint.label}/> )}
+        (<ChartBar value={dataPoint.value} label={dataPoint.label} maxValue={totalMaximum} key={dataPoint.label}/>) )}
     </div>
   )
 }
